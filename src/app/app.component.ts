fed43f741ba1837d819fceaa44253e866ef3499b
Takahiro Suzuki <bur8787@gmail.com>
2017-09-10 19:59:21 +0900

    add ngx-bootstrap/datepicker, ngx-bootstrap/bsdatepicker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {BsDatepickerConfig} from "ngx-bootstrap/datepicker";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,119 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  minDate = new Date(2017, 5, 10);
+  maxDate = new Date(2018, 9, 15);
+  public dt: Date = new Date();
+  public events: any[];
+  public tomorrow: Date;
+  public afterTomorrow: Date;
+  public dateDisabled: {date: Date, mode: string}[];
+  public formats: string[] = ['DD-MM-YYYY', 'YYYY/MM/DD', 'DD.MM.YYYY',
+    'shortDate'];
+  public format: string = this.formats[0];
+  public dateOptions: any = {
+    formatYear: 'YY',
+    startingDay: 1
+  };
+  private opened: boolean = false;
+
+
+  _bsValue: Date;
+
+  locale = 'ja';
+  bsConfig: Partial<BsDatepickerConfig>;
+
+  applyLocale(pop: any) {
+    // create new object on each property change
+    // so Angular can catch object reference change
+    this.bsConfig = Object.assign({}, {locale: this.locale});
+    setTimeout(() => {
+      pop.hide();
+      pop.show();
+    });
+  }
+
+  get bsValue(): Date {
+    return this._bsValue;
+  }
+
+  set bsValue(v: Date) {
+    console.log(v);
+    this._bsValue = v;
+  }
+
+  _bsRangeValue: any = [new Date(2017, 7, 4), new Date(2017, 7, 20)];
+  get bsRangeValue(): any {
+    return this._bsRangeValue;
+  }
+
+  set bsRangeValue(v: any) {
+    this._bsRangeValue = v;
+  }
+
+  log(v: any) {
+    console.log(v);
+  }
+
+  public constructor() {
+    (this.tomorrow = new Date()).setDate(this.tomorrow.getDate() + 1);
+    (this.afterTomorrow = new Date()).setDate(this.tomorrow.getDate() + 2);
+    (this.minDate = new Date()).setDate(this.minDate.getDate() - 1000);
+    (this.dateDisabled = []);
+    this.events = [
+      {date: this.tomorrow, status: 'full'},
+      {date: this.afterTomorrow, status: 'partially'}
+    ];
+  }
+
+  public getDate(): number {
+    return this.dt && this.dt.getTime() || new Date().getTime();
+  }
+
+  public today(): void {
+    this.dt = new Date();
+  }
+
+  public d20090824(): void {
+    this.dt = new Date(2009,7,24);
+  }
+
+  public disableTomorrow(): void {
+    this.dateDisabled = [{date: this.tomorrow, mode: 'day'}];
+  }
+
+  // todo: implement custom class cases
+  public getDayClass(date: any, mode: string): string {
+    if (mode === 'day') {
+      let dayToCheck = new Date(date).setHours(0, 0, 0, 0);
+
+      for (let event of this.events) {
+        let currentDay = new Date(event.date).setHours(0, 0, 0, 0);
+
+        if (dayToCheck === currentDay) {
+          return event.status;
+        }
+      }
+    }
+
+    return '';
+  }
+
+  public disabled(date: Date, mode: string): boolean {
+    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+  }
+
+  public open(): void {
+    this.opened = !this.opened;
+  }
+
+  public clear(): void {
+    this.dt = void 0;
+    this.dateDisabled = undefined;
+  }
+
+  public toggleMin(): void {
+    this.dt = new Date(this.minDate.valueOf());
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import { defineLocale } from 'ngx-bootstrap/bs-moment';
+import { ja } from 'ngx-bootstrap/locale';
+defineLocale('ja', ja);
+
+import {AppComponent} from './app.component';
+
+import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
+import { DatepickerModule } from 'ngx-bootstrap/datepicker';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, BsDatepickerModule.forRoot(), DatepickerModule.forRoot(), BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
